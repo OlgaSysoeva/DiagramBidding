7d9fa5bd1a2ba4e94ba12b7dcbccd2058b1235cb
Olga Sysoeva <sysoevaolya@gmail.com>
2019-05-23 12:52:37 +0300

    Создан контроллер

--- DiagramBidding/Controllers/HomeController.cs
+++ DiagramBidding/Controllers/HomeController.cs
@@ -3,27 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DiagramBidding.Models;
 
 namespace DiagramBidding.Controllers
 {
     public class HomeController : Controller
     {
+        // создаем контекст данных
+        DBContext db = new DBContext();
+
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            IEnumerable<Tool> tools = db.Tools;
+            ViewBag.Books = tools;
             return View();
         }
     }
