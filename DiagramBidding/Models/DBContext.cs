bc11c97cfd8624755530be6f3ecf5780fa9bbca5
Olga Sysoeva <sysoevaolya@gmail.com>
2019-05-23 11:34:23 +0300

    Создан контекст данных

---
+++ DiagramBidding/Models/DBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace DiagramBidding.Models
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<Tool> Tools { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<HandbkTrade> HandbkTrades { get; set; }
+    }
+}
