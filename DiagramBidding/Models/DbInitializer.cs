3dc05ad25de55134b28845eeba61abf22a47f21d
Olga Sysoeva <sysoevaolya@gmail.com>
2019-05-23 21:42:33 +0300

    Скорректировано заполнение данных
    Добавлено поле Id

--- DiagramBidding/Models/DbInitializer.cs
+++ DiagramBidding/Models/DbInitializer.cs
@@ -10,12 +10,12 @@ namespace DiagramBidding.Models
     {
         protected override void Seed(DBContext db)
         {
-            db.Tools.Add(new Tool { Currency = "EUR" });
-            db.Tools.Add(new Tool { Currency = "USD" });
-            db.Tools.Add(new Tool { Currency = "RUB" });
+            db.Tools.Add(new Tool { Id = 1, Currency = "EUR" });
+            db.Tools.Add(new Tool { Id = 2, Currency = "USD" });
+            db.Tools.Add(new Tool { Id = 3, Currency = "RUB" });
 
-            db.Members.Add(new Member { Name = "Егоров Владимир Васильевич" });
-            db.Members.Add(new Member { Name = "Холодкова Ангелина Валерьевна" });
+            db.Members.Add(new Member { Id = 1, Name = "Егоров Владимир Васильевич" });
+            db.Members.Add(new Member { Id = 2, Name = "Холодкова Ангелина Валерьевна" });
 
             db.HandbkTrades.Add(new HandbkTrade { ToolId = 1, Price = 5.20, Size = 6, Date = new DateTime(2019, 1, 15, 18, 30, 25), CustomerId = 1, SellerId = 2 });
             db.HandbkTrades.Add(new HandbkTrade { ToolId = 2, Price = 3.50, Size = 3, Date = new DateTime(2019, 3, 2, 13, 40, 12), CustomerId = 1, SellerId = 2 });
