3320c141a76cb574220ad224293f8763a3e536ff
Olga Sysoeva <sysoevaolya@gmail.com>
2019-05-26 12:37:42 +0300

    Добавлены аннотации для имени полей

--- DiagramBidding/Models/HandbkTrade.cs
+++ DiagramBidding/Models/HandbkTrade.cs
@@ -15,18 +15,24 @@ namespace DiagramBidding.Models
         public int Id { get; set; }
         // ID валюты
         [ForeignKey(nameof(Tool))]
+        [Display(Name ="Валюта")]
         public int? ToolId { get; set; }
         // цена
+        [Display(Name = "Цена")]
         public double Price { get; set; }
         // объем
+        [Display(Name = "Количество")]
         public int Size { get; set; }
         // время сделки
+        [Display(Name = "Дата")]
         public DateTime Date { get; set; }
         // ID участника-покупателя
         [ForeignKey(nameof(Customer))]
+        [Display(Name = "Покупатель")]
         public int? CustomerId { get; set; }
         // ID участника-продавца
         [ForeignKey(nameof(Seller))]
+        [Display(Name = "Продавец")]
         public int? SellerId { get; set; }
 
         public virtual Tool Tool { get; set; }
